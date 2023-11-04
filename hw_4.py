5b0f8874cffa03ad632b8a0259e102e778f8b089
Night6rW0rk <146623149+Asurusgg@users.noreply.github.com>
2023-11-05 00:11:38 +0300

    Create hw_4.py

---
+++ hw_4.py
@@ -0,0 +1,18 @@
+# НОМЕР 1
+size = int(input("Введите размер доски: "))
+for i in range(size):
+    for j in range(size):
+        if (i + j) % 2 == 0:
+            print("#", end=' ')
+        else:
+            print(" ", end=' ')
+    print()
+
+# НОМЕР 2
+height = int(input("Введите высоту пирамиды: "))
+for i in range(1, height + 1):
+    for j in range(height - i):
+        print(" ", end='')
+    for k in range(1, 2 * i):
+        print(i, end='')
+    print()
